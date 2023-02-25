8a0da8a419b68db6a14fd2948798dea168dc9a35
arperevalov <lovetraindriver@gmail.com>
2023-02-25 08:54:56 +0700

    fix: edit header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,36 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState } from "react";
 
 export default function Header () {
 
     const router = useRouter();
-    const [active, setActive] = useState(false);
-
-    const toggleHeader = () => {
-        setActive(!active)
-    }
 
     return <>
-        <header className={`header ${active ? 'active' : ''}`}>
-            <div className="header__dropdown">
+        <header className={`header`}>
+            { router.pathname === '/' ? <>
+                <div className="header__top">
+                    <div className="header__logo">Friska</div>
+                </div>
+            </> : ''}
+
+            <div className="header__bottom">
                 <nav className="header__nav">
-                    <Link className={`header__link ${router.pathname === '/' ? 'active' : '' } h3`} href={'/'}>Lists</Link>
-                    <Link className={`header__link ${router.pathname === '/settings' ? 'active' : '' } h3`} href={'/settings'}>Settings</Link>
+                    <Link className={`header__link header__link--new h3`} href={'/new'}>
+                        <img src="/svg/new.svg" alt="" />
+                        <span className="visually-hidden">New Item</span>
+                    </Link>
+                    <Link className={`header__link h3`} href={'/'}>
+                        <img src="/svg/lists.svg" alt="" />
+                        <span className="visually-hidden">Lists</span>
+                    </Link>
+                    <Link className={`header__link h3`} href={'/settings'}>
+                        <img src="/svg/settings.svg" alt="" />
+                        <span className="visually-hidden">Settings</span>
+                    </Link>
                 </nav>
             </div>
-            <button type="button" className="header__logo" onClick={toggleHeader}>Friska</button>
+            
         </header>
     </>
 }
