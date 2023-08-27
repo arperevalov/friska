511bf533091037f06d30a55a1991355f4edca4d8
arperevalov <lovetraindriver@gmail.com>
2023-08-27 15:51:10 +0700

    fix: tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,42 +8,42 @@ import useCards from '@/hooks/useCards'
 
 export default function Home() {
 
-  const { lists, cards } = useMainStore(store => store)
+	const { lists, cards } = useMainStore(store => store)
 
-  useLists()
-  useCards()
+	useLists()
+	useCards()
 
-  return (
-    <>
-      <Head>
-        <title>Friska</title>
-        <meta name="description" content="Storage management app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header/>
-      <main>
-        <div className="container">
-          { lists?.length > 0 ? lists.map(list => {
-            return (
-              <div className="list" key={list.id}>
-                <div className="list__header">
-                  <h2 className='list__title'>{list.title}</h2>
-                </div>
-                <div className="list__items">
-                  { cards.length > 0 ? cards.map((card: CardInterface) => {
-                    if (card.listId === list.id) {
-                      return (
-                        <Card {...card} key={card.id} />
-                      )
-                    }
-                  }) : '' }
-                </div>
-              </div>
-            )
-          }) : '' }
-        </div>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Friska</title>
+				<meta name="description" content="Storage management app" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Header/>
+			<main>
+				<div className="container">
+				{ lists?.length > 0 ? lists.map(list => {
+					return (
+					<div className="list" key={list.id}>
+						<div className="list__header">
+						<h2 className='list__title'>{list.title}</h2>
+						</div>
+						<div className="list__items">
+						{ cards.length > 0 ? cards.map((card: CardInterface) => {
+							if (card.listId === list.id) {
+							return (
+								<Card {...card} key={card.id} />
+							)
+							}
+						}) : '' }
+						</div>
+					</div>
+					)
+				}) : '' }
+				</div>
+			</main>
+		</>
+	)
 }
--- store/MainStore.ts
+++ store/MainStore.ts
@@ -11,32 +11,32 @@ interface MainStoreInterface {
     addList: CallableFunction
   }
   
-  export const useMainStore = create<MainStoreInterface>(set => ({
+export const useMainStore = create<MainStoreInterface>(set => ({
     lists: [],
     cards: [],
     setCards: (cards:CardInterface[]) => set((state) => {
-      const newCards = cards.map( (card, index) => {
-        const id = card.id ? card.id : state.cards.length + index
-        return {...card, id}
-      });
-      return {cards: [...state.cards, ...newCards]}
+		const newCards = cards.map( (card, index) => {
+			const id = card.id ? card.id : state.cards.length + index
+			return {...card, id}
+		});
+		return {cards: [...state.cards, ...newCards]}
     }),
     updateCard: (card: CardInterface) => set((state) => {
-      const index = state.cards.findIndex(item => {
-        return item.id === card.id;
-      })
-      state.cards[index] = card;
-      return {cards: [...state.cards]}
+		const index = state.cards.findIndex(item => {
+			return item.id === card.id;
+		})
+		state.cards[index] = card;
+		return {cards: [...state.cards]}
     }),
     setLists: (lists:ListInterface[]) => set((state) => {
-      return {lists}
+      	return {lists}
     }),
     addList: (list: ListInterface) => set((state)=> {
-      return {
-        lists: [
-          ...state.lists,
-          list
-        ]
-      }
+		return {
+			lists: [
+			...state.lists,
+			list
+			]
+		}
     })
-  }))
+}))
