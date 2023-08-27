d800bd5b94cac31ca8e9dace63a876baf651f26a
arperevalov <lovetraindriver@gmail.com>
2023-08-27 16:14:55 +0700

    fix: useCards, useLists length check

--- hooks/useLists.tsx
+++ hooks/useLists.tsx
@@ -2,13 +2,15 @@ import { useMainStore } from "@/store/MainStore";
 import { useEffect } from "react";
 
 export default function useLists () {
-    const {setLists} = useMainStore(state=>state);
+    const {setLists, lists} = useMainStore(state=>state);
 
     useEffect(()=>{
+        if (lists.length > 0) return;
+        
         const requestLists = async () =>{
-        const request = await fetch('/api/lists')
-        const requestJSON = await request.json();
-        setLists(requestJSON.lists)
+            const request = await fetch('/api/lists')
+            const requestJSON = await request.json();
+            setLists(requestJSON.lists)
         }
         requestLists();
     },[])
