e21131ec589939782d1997b3e387a8db75af99e8
arperevalov <lovetraindriver@gmail.com>
2023-08-29 06:11:42 +0700

    fix: img to Image at header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -10,7 +11,7 @@ export default function Header () {
             <div className="header__top">
                 { router.pathname === '/' ? <>
                     <div className="header__logo">    
-                        <img className="header__logo-icon" src="/friska.svg" alt="" />
+                        <Image className="header__logo-icon" src="/friska.svg" alt="" />
                         Friska
                     </div>
                 </> : <></>}
@@ -19,15 +20,15 @@ export default function Header () {
             <div className="header__bottom">
                 <nav className="header__nav">
                     <Link className={`header__link${router.pathname === '/new' ? ' header__link--active' : ''}`} href={'/new'}>
-                        <img src="/svg/new.svg" alt="" />
+                        <Image src="/svg/new.svg" alt="" />
                         <span className="visually-hidden">New Item</span>
                     </Link>
                     <Link className={`header__link${router.pathname === '/' ? ' header__link--active' : ''}`} href={'/'}>
-                        <img src="/svg/lists.svg" alt="" />
+                        <Image src="/svg/lists.svg" alt="" />
                         <span className="visually-hidden">Lists</span>
                     </Link>
                     <Link className={`header__link${router.pathname === '/settings' ? ' header__link--active' : ''}`} href={'/settings'}>
-                        <img src="/svg/settings.svg" alt="" />
+                        <Image src="/svg/settings.svg" alt="" />
                         <span className="visually-hidden">Settings</span>
                     </Link>
                 </nav>
