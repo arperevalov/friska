da5e5fae52074e99a0036a4fbcb169525861db7a
arperevalov <lovetraindriver@gmail.com>
2023-08-29 07:21:00 +0700

    fix: prettier Select

--- components/Select.tsx
+++ components/Select.tsx
@@ -3,41 +3,42 @@ import ListInterface from "@/interfaces/List";
 import { useEffect, useState } from "react";
 
 export interface InputInterface {
-    label: string,
-    values: ListInterface[]
-    defaultValue?: number ,
-    setFormData: CallableFunction,
-    formKey: string,
-    required?: boolean
+    label: string;
+    values: ListInterface[];
+    defaultValue?: number;
+    setFormData: CallableFunction;
+    formKey: string;
+    required?: boolean;
 }
 
 export const Select = (props: InputInterface) => {
     const { label, defaultValue, values, setFormData, formKey, required } = props;
     const [inputValue, setInputValue] = useState(defaultValue ? defaultValue : 0);
 
-    useEffect(()=>{
-        setFormData((previousValue: CardInterface)=>({...previousValue, [formKey]: inputValue}))
-    },[])
+    useEffect(() => {
+        setFormData((previousValue: CardInterface) => ({ ...previousValue, [formKey]: inputValue }));
+    }, []);
 
     const onChange = (event: React.FormEvent<HTMLSelectElement>) => {
         const value = parseInt(event.currentTarget.value);
         setInputValue(value);
-        setFormData((previousValue: CardInterface)=>({...previousValue, [formKey]: value}))
-    }
+        setFormData((previousValue: CardInterface) => ({ ...previousValue, [formKey]: value }));
+    };
 
-    return <>
-        <label htmlFor="" className="input">
-            <span className="input__label">{label}</span>
-            <select defaultValue={defaultValue} onChange={onChange} className="input__input" required={required}>
-                {values.map(value => {
-                    return <option 
-                        key={value.id} 
-                        value={value.id} 
-                        >
-                            {value.title}
-                        </option>
-                })}
-            </select>
-        </label>
-    </>
-}
+    return (
+        <>
+            <label htmlFor="" className="input">
+                <span className="input__label">{label}</span>
+                <select defaultValue={defaultValue} onChange={onChange} className="input__input" required={required}>
+                    {values.map((value) => {
+                        return (
+                            <option key={value.id} value={value.id}>
+                                {value.title}
+                            </option>
+                        );
+                    })}
+                </select>
+            </label>
+        </>
+    );
+};
