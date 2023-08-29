5f7b6fb70e32c0e8bde3374ea12abfb85a8dd051
arperevalov <lovetraindriver@gmail.com>
2023-08-29 07:21:39 +0700

    fix: prettier clean

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,25 +1,4 @@
 module.exports = {
-    'overrides': [
-        {
-            'files': '*.scss',
-            'options': {
-                'singleQuote': false,
-            },
-            'parser': 'postcss-scss',
-        },
-        {
-            'files': '*.js',
-            'options': {
-                'singleQuote': true,
-            },
-            'parser': '@babel/eslint-parser',
-            'parserOptions': {
-                'babelOptions': {
-                    'configFile': './babel.config.js',
-                },
-            },
-        },
-    ],
     'printWidth': 120,
     'quoteProps': 'preserve',
     'tabWidth': 4,
