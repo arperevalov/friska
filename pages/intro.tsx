6c65e807d196f4267460eef75a118daffa061fa0
arperevalov <lovetraindriver@gmail.com>
2023-09-05 07:51:03 +0700

    fix: codestyle

--- pages/intro.tsx
+++ pages/intro.tsx
@@ -2,13 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Intro() {
-
     return (
         <>
             <main>
-                <div className="intro-cards">
-
-                </div>
+                <div className="intro-cards"></div>
                 <div className="container">
                     <div className="intro">
                         <div className="intro__logo">
@@ -17,17 +14,16 @@ export default function Intro() {
                             </div>
                             Friska
                         </div>
-                        <h1 className="intro__title">
-                            Hello, there
-                        </h1>
+                        <h1 className="intro__title">Hello, there</h1>
                         <p className="intro__text">
-                            Friska is a simple and intuitive web application designed to help you manage the expiry dates of your food and medicine.
+                            Friska is a simple and intuitive web application designed to help you manage the expiry
+                            dates of your food and medicine.
                         </p>
                         <div className="btn-row">
-                            <Link href={'./sign-in'} className="btn btn--sm btn--primary">
+                            <Link href={"./sign-in"} className="btn btn--sm btn--primary">
                                 Sign-in
                             </Link>
-                            <Link href={'./sign-in'} className="btn btn--sm btn--outline-primary">
+                            <Link href={"./sign-in"} className="btn btn--sm btn--outline-primary">
                                 Sign-up
                             </Link>
                         </div>
