1bc3157e4ab12c11f1c957832beff7661e5fdddb
arperevalov <lovetraindriver@gmail.com>
2023-10-31 20:16:21 +0700

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,23 +24,23 @@ export default function Home() {
             <main>
                 <div className="container">
                     {lists.map((list) => {
-                              return (
-                                  <div className="list" key={list.id}>
-                                      <div className="list__header">
-                                          <h2 className="list__title">{list.title}</h2>
-                                      </div>
-                                      <div className="list__items">
-                                          {cards.length > 0
-                                              ? cards.map((card: CardInterface) => {
-                                                    if (card.listId === list.id) {
-                                                        return <Card {...card} key={card.id} />;
-                                                    }
-                                                })
-                                              : ""}
-                                      </div>
-                                  </div>
-                              );
-                          })}
+                        return (
+                            <div className="list" key={list.id}>
+                                <div className="list__header">
+                                    <h2 className="list__title">{list.title}</h2>
+                                </div>
+                                <div className="list__items">
+                                    {cards.length > 0
+                                        ? cards.map((card: CardInterface) => {
+                                              if (card.listId === list.id) {
+                                                  return <Card {...card} key={card.id} />;
+                                              }
+                                          })
+                                        : ""}
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </main>
         </>
