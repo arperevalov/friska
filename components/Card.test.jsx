970a0d07d269c779ec6f0bf7f54c3b74b3c0e774
arperevalov <lovetraindriver@gmail.com>
2023-10-31 19:09:12 +0700

    feat: Card rendering tests

--- components/Card.test.jsx
+++ components/Card.test.jsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Card } from '@/components/Card';
 
 const cardData = {
     id: 0,
     title: "Milk",
-    expDate: "Tue Sep 12 2023 23:39:55 GMT+0700 (Krasnoyarsk Standard Time)",
     left: 1,
     units: "kg",
     listId: 0
 };
 
+const currentDate = new Date();
+const oldDate = "Tue Sep 12 1999 23:39:55 GMT+0700 (Krasnoyarsk Standard Time)";
+
 describe('Card component', () => {
     it('renders with data', () => {
-        const { getByText } = render(<Card {...cardData}/>);
+        const { getByText } = render(<Card {...cardData} expDate={currentDate}/>);
 
         const titleText = getByText(/Milk/i);
-        const expDateText = getByText(/9\/12\/2023/i);
+        const expDateText = getByText(currentDate.toLocaleDateString());
         const leftText = getByText(/1 kg/i);
 
         expect(titleText).toBeInTheDocument();
         expect(expDateText).toBeInTheDocument();
         expect(leftText).toBeInTheDocument();
+    });
+
+    it('renders fresh', () => {
+        const { container } = render(<Card {...cardData} expDate={currentDate}/>);
+
+        const outdated = container.querySelector('.card').classList.contains('outdated');
+        expect(outdated).toBeFalsy();
+    });
+
+    it('renders outdated', () => {
+        const { container } = render(<Card {...cardData} expDate={oldDate}/>);
+
+        const outdated = container.querySelector('.card').classList.contains('outdated');
+        expect(outdated).toBeTruthy();
+    });
+
+    it('toggles card active state', () => {
+        const { container } = render(<Card {...cardData} expDate={currentDate}/>);
+
+        const card = container.querySelector('.card');
+        const cardContainer = container.querySelector('.card__container');
+
+        fireEvent.click(cardContainer);
+        const active = card.classList.contains('active');
+        expect(active).toBeTruthy();
+
+        fireEvent.click(cardContainer);
+        const activeSecond = card.classList.contains('active');
+        expect(activeSecond).toBeFalsy();
     });
 });
