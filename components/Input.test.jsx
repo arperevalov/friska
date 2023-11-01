3fb3adad5206c3515324d7f4f8f71dbf28c49543
arperevalov <lovetraindriver@gmail.com>
2023-11-01 07:57:18 +0700

    .

--- components/Input.test.jsx
+++ components/Input.test.jsx
@@ -2,38 +2,37 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { Input } from "@/components/Input";
 
-
 const data = {
-    label: 'Some label',
+    label: "Some label",
     setFormData: () => {},
-    formKey: 'random',
-}
+    formKey: "random",
+};
 
 const dataNumber = {
     defaultValue: 2,
-    type: 'number',
+    type: "number",
     min: 2,
     max: 5,
-}
+};
 
 const dataText = {
-    defaultValue: '',
-    type: 'text',
-}
+    defaultValue: "",
+    type: "text",
+};
 
 describe("Input component", () => {
     it("renders number type", () => {
-        const { container, getByText } = render(<Input {...data} {...dataNumber}/>);
+        const { container, getByText } = render(<Input {...data} {...dataNumber} />);
 
         const labelText = getByText(/some label/i);
 
-        const input = container.querySelector('input');
-        const inputTypeNumber = input.getAttribute('type') === 'number';
+        const input = container.querySelector("input");
+        const inputTypeNumber = input.getAttribute("type") === "number";
 
-        const inputMin = parseInt(input.getAttribute('min'), 10);
-        const inputMax = parseInt(input.getAttribute('max'), 10);
+        const inputMin = parseInt(input.getAttribute("min"), 10);
+        const inputMax = parseInt(input.getAttribute("max"), 10);
         const inputMinAndMaxSet = inputMin === dataNumber.min && inputMax === dataNumber.max;
-        
+
         const inputValue = parseInt(input.value, 10);
 
         expect(labelText).toBeInTheDocument();
@@ -43,13 +42,13 @@ describe("Input component", () => {
     });
 
     it("renders text type", () => {
-        const { container, getByText } = render(<Input {...data} {...dataText}/>);
+        const { container, getByText } = render(<Input {...data} {...dataText} />);
 
         const labelText = getByText(/some label/i);
 
-        const input = container.querySelector('input');
-        const inputTypeText = input.getAttribute('type') === 'text';
-        
+        const input = container.querySelector("input");
+        const inputTypeText = input.getAttribute("type") === "text";
+
         const inputValue = input.value;
 
         expect(labelText).toBeInTheDocument();
