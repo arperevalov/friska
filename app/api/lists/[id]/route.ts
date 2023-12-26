fe678603918911be04aa6e55ef82f8a7a6d575dd
arperevalov <lovetraindriver@gmail.com>
2023-12-26 23:38:41 +0700

    feat: api routes methods

--- app/api/lists/[id]/route.ts
+++ app/api/lists/[id]/route.ts
@@ -14,3 +14,36 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
         }
     }
 }
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const id = parseInt(params.id, 10);
+        const requestData = await req.json();
+        const request = await axios.put(`${process.env.NEXT_PUBLIC_SERVER_URL}/lists/${id}`, requestData, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await request.data;
+
+        return Response.json(data);
+    } catch (error: unknown) {
+        if (error instanceof AxiosError) {
+            return Response.json({ error: "Server error" }, { status: error.status });
+        }
+    }
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const id = parseInt(params.id, 10);
+        const request = await axios.delete(`${process.env.NEXT_PUBLIC_SERVER_URL}/lists/${id}`);
+        const data = await request.data;
+
+        return Response.json(data);
+    } catch (error: unknown) {
+        if (error instanceof AxiosError) {
+            return Response.json({ error: "Server error" }, { status: error.status });
+        }
+    }
+}
