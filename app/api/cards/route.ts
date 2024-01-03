383d2d3e95c4dff3e66133bcb38f1266baa8a594
arperevalov <lovetraindriver@gmail.com>
2024-01-03 11:16:55 +0700

    feat: api routes auth-token

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -1,8 +1,14 @@
 import axios, { AxiosError } from "axios";
+import { cookies } from "next/headers";
 
 export async function GET() {
+    const token = cookies().get("auth-token")?.value;
     try {
-        const request = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/cards`);
+        const request = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/cards`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            },
+        });
         const data = await request.data;
 
         return Response.json(data);
@@ -14,11 +20,13 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
+    const token = cookies().get("auth-token")?.value;
     try {
         const requestData = await req.json();
         const request = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/cards`, requestData, {
             headers: {
                 "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
             },
         });
 
--- app/api/lists/[id]/route.ts
+++ app/api/lists/[id]/route.ts
@@ -1,10 +1,16 @@
 import axios, { AxiosError } from "axios";
+import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+    const token = cookies().get("auth-token")?.value;
     try {
         const id = parseInt(params.id, 10);
-        const request = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/lists/${id}`);
+        const request = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/lists/${id}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            },
+        });
         const data = await request.data[0];
 
         return Response.json(data);
@@ -16,12 +22,14 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
 }
 
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    const token = cookies().get("auth-token")?.value;
     try {
         const id = parseInt(params.id, 10);
         const requestData = await req.json();
         const request = await axios.put(`${process.env.NEXT_PUBLIC_SERVER_URL}/lists/${id}`, requestData, {
             headers: {
                 "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
             },
         });
         const data = await request.data;
@@ -35,9 +43,14 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
 }
 
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+    const token = cookies().get("auth-token")?.value;
     try {
         const id = parseInt(params.id, 10);
-        const request = await axios.delete(`${process.env.NEXT_PUBLIC_SERVER_URL}/lists/${id}`);
+        const request = await axios.delete(`${process.env.NEXT_PUBLIC_SERVER_URL}/lists/${id}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            },
+        });
         const data = await request.data;
 
         return Response.json(data);
