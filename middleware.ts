d4d920debd2becda7879a88d40ae4545d0e66d62
arperevalov <lovetraindriver@gmail.com>
2024-01-03 10:36:54 +0700

    feat: middleware route

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const routes = /\/sign-in|\/sign-up|\/intro/gi
+
+export function middleware(request: NextRequest) {
+    const isAuth = request.cookies.get("auth-token")?.value || false;
+
+    if (isAuth && request.nextUrl.pathname.match(routes)) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    if (!isAuth && !request.nextUrl.pathname.match(routes)) {
+        return NextResponse.redirect(new URL("/intro", request.url));
+    }
+}
+
+export const config = {
+    matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+};
