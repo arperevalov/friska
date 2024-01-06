db0a4fc799e5791fd5ff1c5e68db4e90013561ef
arperevalov <lovetraindriver@gmail.com>
2024-01-06 18:30:18 +0700

    refactor: ModalsProvider state

--- components/Modals.tsx
+++ components/Modals.tsx
@@ -6,27 +6,23 @@ import ModalTitle from "@/components/modal/ModalTitle";
 import useModals from "@/hooks/useModals";
 import FormUpdateCard from "./forms/FormUpdateCard";
 import FormNewList from "./forms/FormNewList";
+import { ModalStateItemInterface } from "@/contexts/ModalContext";
 
 interface ModalsProps {
-    modals: {
-        [ModalsEnum.FormNewList]: {
-            state: boolean;
-            parameters: null;
-        };
-        [ModalsEnum.FormNewCard]: {
-            state: boolean;
-            parameters: null;
-        };
-        [ModalsEnum.FormUpdateCard]: {
-            state: boolean;
-            parameters: number | null;
-        };
-    };
+    modals: ModalStateItemInterface[];
 }
 
 export default function Modals(props: ModalsProps) {
     const { modals } = props;
     const { toggleModalAction } = useModals();
+
+    const getModalStateItem = (name: ModalsEnum) => {
+        const index = modals.findIndex((item) => {
+            return item.name === name;
+        })
+
+        return modals[index];
+    }
 
     const hideFormNewList = () => {
         toggleModalAction(ModalsEnum.FormNewList);
@@ -42,25 +38,25 @@ export default function Modals(props: ModalsProps) {
 
     return (
         <>
-            <Modal show={modals[ModalsEnum.FormNewList].state} onHide={hideFormNewList}>
+            <Modal show={getModalStateItem(ModalsEnum.FormNewList).state} onHide={hideFormNewList}>
                 <ModalHeader>
                     <ModalTitle>New list</ModalTitle>
                 </ModalHeader>
                 <FormNewList onSubmit={hideFormNewList} />
             </Modal>
-            <Modal show={modals[ModalsEnum.FormNewCard].state} onHide={hideFormNewCard}>
+            <Modal show={getModalStateItem(ModalsEnum.FormNewCard).state} onHide={hideFormNewCard}>
                 <ModalHeader>
                     <ModalTitle>New card</ModalTitle>
                 </ModalHeader>
                 <FormNewCard onSubmit={hideFormNewCard} />
             </Modal>
-            <Modal show={modals[ModalsEnum.FormUpdateCard].state} onHide={hideFormUpdateCard}>
+            <Modal show={getModalStateItem(ModalsEnum.FormUpdateCard).state} onHide={hideFormUpdateCard}>
                 <ModalHeader>
                     <ModalTitle>New card</ModalTitle>
                 </ModalHeader>
                 <FormUpdateCard
                     onSubmit={hideFormUpdateCard}
-                    parameters={modals[ModalsEnum.FormUpdateCard]?.parameters}
+                    parameters={getModalStateItem(ModalsEnum.FormUpdateCard).parameters}
                 />
             </Modal>
         </>
--- contexts/ModalContext.tsx
+++ contexts/ModalContext.tsx
@@ -7,35 +7,46 @@ interface ModalContextValue {
     toggleModal: CallableFunction;
 }
 
+export interface ModalStateItemInterface {
+    name: ModalsEnum,
+    state: boolean,
+    parameters: null | string | number,
+}
+
 const defaultValue: ModalContextValue = {
     toggleModal: () => {},
 };
 
 export const ModalContext = createContext<ModalContextValue>(defaultValue);
 export default function ModalProvider({ children }: { children: React.ReactNode }) {
-    const [modals, setModal] = useState({
-        [ModalsEnum.FormNewList]: {
+    const [modals, setModal] = useState<ModalStateItemInterface[]>([
+        {
+            name: ModalsEnum.FormNewList,
+            state: false,
+            parameters: null,
+        },{
+            name: ModalsEnum.FormNewCard,
+            state: false,
+            parameters: null,
+        },{
+            name: ModalsEnum.FormUpdateCard,
             state: false,
             parameters: null,
         },
-        [ModalsEnum.FormNewCard]: {
-            state: false,
-            parameters: null,
-        },
-        [ModalsEnum.FormUpdateCard]: {
-            state: false,
-            parameters: null,
-        },
-    });
+    ]);
 
     const toggleModal = (name: ModalsEnum, parameters: string | number | null) => {
-        setModal((previous) => ({
-            ...previous,
-            [name]: {
-                state: !previous[name].state,
-                parameters,
-            },
-        }));
+        const value = modals.map((item) => {
+            if (item.name === name) {
+                return {
+                    ...item,
+                    state: !item.state,
+                    parameters
+                }
+            }
+            return item;
+        })
+        setModal(value);
     };
 
     return (
