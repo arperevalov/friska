1ecb2afd27c8650f8e185b0fdc774b360afa4df0
arperevalov <lovetraindriver@gmail.com>
2024-01-07 12:07:28 +0700

    feat: form change password at settigns

--- app/(main)/settings/password/page.tsx
+++ app/(main)/settings/password/page.tsx
@@ -1,13 +1,45 @@
 "use client";
 
 import Header from "@/components/Header";
+import { Input } from "@/components/Input";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface FormData {
+    password: string;
+    password_repeat: string;
+}
 
 export default function Settings() {
+    const { register, handleSubmit } = useForm<FormData>();
+    const { } = useCurrentUser();
+
+    const onFormSubmit: SubmitHandler<FormData> = () => {
+        //
+    }
+
     return (
         <>
             <Header title="Change Password" />
             <main>
-                <div className="container"></div>
+                <div className="container">
+                    <form onSubmit={handleSubmit(onFormSubmit)}>
+                        <Input
+                            type="password"
+                            label="Password"
+                            register={register}
+                            formKey="password"
+                            required={true}
+                        />
+                        <Input
+                            type="password"
+                            label="Repeat password"
+                            register={register}
+                            formKey="passwordSecond"
+                            required={true}
+                        />
+                    </form>
+                </div>
             </main>
         </>
     );
