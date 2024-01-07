f9eccc1ea021872f77785443219acfd3444e1df7
arperevalov <lovetraindriver@gmail.com>
2024-01-07 11:59:45 +0700

    feat: settings password route

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from "@/components/Header";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Link from "next/link";
 
 export default function Settings() {
     const { currentUser } = useCurrentUser();
@@ -17,6 +18,13 @@ export default function Settings() {
                             <div className="user-main__username">{currentUser.username}</div>
                             <div className="user-main__email">{currentUser.email}</div>
                         </div>
+                    </div>
+                    <div className="settings">
+                        <ul className="settings__list">
+                            <li className="settings__item">
+                                <Link href="/settings/password" className="settings__link">Change password</Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </main>
---
+++ app/(main)/settings/password/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Header from "@/components/Header";
+
+export default function Settings() {
+
+    return (
+        <>
+            <Header title="Change Password" />
+            <main>
+                <div className="container">
+                </div>
+            </main>
+        </>
+    );
+}
