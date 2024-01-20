221e1992587fabf2a4e1da2aba57ac7ab31238ae
arperevalov <lovetraindriver@gmail.com>
2024-01-20 23:55:59 +0700

    fix: index page test

--- app/(main)/page.test.jsx
+++ app/(main)/page.test.jsx
@@ -2,38 +2,42 @@ import React from "react";
 import { render } from "@testing-library/react";
 import useLists from "@/hooks/useLists";
 import useCards from "@/hooks/useCards";
-import Home from "@/app/page";
+import Home from "@/app/(main)/page";
 
 jest.mock("@/hooks/useLists");
 jest.mock("@/hooks/useCards");
+
+const dateString = new Date().toISOString().replace(/(\d)T(\d.{0,})\.\d{0,}Z/, "$1 $2");
 
 const mockStore = {
     cards: [
         {
             id: 0,
-            title: "Milk",
-            exp_date: "Tue Sep 12 2023 23:39:55 GMT+0700 (Krasnoyarsk Standard Time)",
-            left_count: 1,
-            units: "kg",
+            units: "l",
             list_id: 0,
+            title: "Oranges",
+            units: "kg",
+            left_count: 1,
+            user_id: 1,
+            exp_date: dateString,
         },
         {
             id: 1,
-            title: "sss",
-            exp_date: "Tue Sep 12 2023 23:39:55 GMT+0700 (Krasnoyarsk Standard Time)",
-            left_count: 1,
             units: "l",
             list_id: 1,
+            title: "Juice",
+            units: "l",
+            left_count: 1,
+            user_id: 1,
+            list_id: 0,
+            exp_date: dateString,
         },
     ],
     lists: [
         {
-            title: "Fridge",
             id: 0,
-        },
-        {
-            title: "Shelf",
-            id: 1,
+            title: "List Title",
+            best_before: 2,
         },
     ],
 };
@@ -43,12 +47,12 @@ const emptyStore = {
     lists: [],
 };
 
-beforeEach(() => {
-    useLists.mockClear();
-    useCards.mockClear();
-});
-
 describe("Home page", () => {
+    beforeEach(() => {
+        useCards.mockClear();
+        useLists.mockClear();
+    });
+
     it("renders without data", () => {
         useLists.mockReturnValue({ lists: emptyStore.lists });
         useCards.mockReturnValue({ lists: emptyStore.cards });
@@ -65,8 +69,8 @@ describe("Home page", () => {
 
         const { getByText } = render(<Home />);
 
-        const listText = getByText(/fridge/i);
-        const cardText = getByText(/milk/i);
+        const listText = getByText(/list title/i);
+        const cardText = getByText(/oranges/i);
         expect(listText).toBeInTheDocument();
         expect(cardText).toBeInTheDocument();
     });
