a08e70d3e46ddec15dd33fa0e5f1113cad57c5b4
arperevalov <lovetraindriver@gmail.com>
2024-02-06 00:08:28 +0700

    fix: btn-arrow-back to sprite

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,15 +44,7 @@ export default function Header(props: HeaderProps) {
                                         className="header__button"
                                     >
                                         <span className="visually-hidden">Go back</span>
-                                        <svg
-                                            width="9"
-                                            height="14"
-                                            viewBox="0 0 9 14"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path d="M8 1L2 7L8 13" stroke="black" strokeWidth="2" />
-                                        </svg>
+                                        <Sprite className="header__icon" name={'btn-arrow-back'}/>
                                     </button>
                                 </div>
                                 <h1 className="header__title">{title}</h1>
