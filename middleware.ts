5d6921949c116fc0bc9f50ead8a073c1737f3b78
arperevalov <lovetraindriver@gmail.com>
2024-02-27 21:04:35 +0700

    .

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import middlewares from "./middlewares";
 
 export async function middleware(request: NextRequest) {
     const nextResponse = NextResponse.next();
-    
-    const middlewareFunctions = middlewares.map(fn => fn(request));
+
+    const middlewareFunctions = middlewares.map((fn) => fn(request));
     const middlewareHeader = [];
 
     for (const middleware of middlewareFunctions) {
@@ -20,8 +20,8 @@ export async function middleware(request: NextRequest) {
     let redirectTo = null;
 
     middlewareHeader.some((header) => {
-        const redirect = header.get('x-middleware-request-redirect');
-        
+        const redirect = header.get("x-middleware-request-redirect");
+
         if (redirect) {
             redirectTo = redirect;
             return true;
@@ -31,7 +31,7 @@ export async function middleware(request: NextRequest) {
 
     if (redirectTo) {
         return NextResponse.redirect(new URL(redirectTo, request.url), {
-        status: 307,
+            status: 307,
         });
     }
 
