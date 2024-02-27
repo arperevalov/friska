e0a8c1de00814497724453cedf7a30db8313029a
arperevalov <lovetraindriver@gmail.com>
2024-02-27 18:07:35 +0700

    fix: middleware isAuth

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 const routes = /\/(sign-in|sign-up|intro)/gi;
 
 export function middleware(request: NextRequest) {
-    const isAuth = request.cookies.get("auth-token")?.value || false;
+    const isAuth = request.cookies.get("auth-token")?.value ?? false;
 
     if (isAuth && request.nextUrl.pathname.match(routes)) {
         return NextResponse.redirect(new URL("/", request.url));
