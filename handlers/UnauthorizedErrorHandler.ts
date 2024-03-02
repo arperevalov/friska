2eed16f0351ce1265e5d7b7fafe3ffcb71d84bd1
arperevalov <lovetraindriver@gmail.com>
2024-03-02 19:26:01 +0700

    fix: UnauthorizedErrorHandler window.location.href

--- handlers/UnauthorizedErrorHandler.ts
+++ handlers/UnauthorizedErrorHandler.ts
@@ -2,5 +2,5 @@ import { deleteCookie } from "cookies-next";
 
 export default function UnauthorizedErrorHandler() {
     deleteCookie("auth-token");
-    window.location.href = "/login";
+    window.location.href = "/sign-in";
 }
