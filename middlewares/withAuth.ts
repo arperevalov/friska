e4b68aaa11cf284ba60fe1c775faf8c1b826041f
arperevalov <lovetraindriver@gmail.com>
2024-03-11 00:25:03 +0700

    feat: withAuth middleware check confirmed

--- middlewares/withAuth.ts
+++ middlewares/withAuth.ts
@@ -4,17 +4,28 @@ const matcher = /(api|_next\/static|_next\/image|favicons|img|js)/gi;
 const routes = /\/(sign-in|sign-up|intro)/gi;
 
 export default async function withAuth(request: NextRequest) {
-    if (request.nextUrl.pathname.match(matcher)) {
+    const { pathname } = request.nextUrl;
+    if (pathname.match(matcher)) {
         return NextResponse.next();
     }
 
     const isAuth = request.cookies.get("auth-token")?.value ?? false;
+    const isConfirmedValue = request.cookies.get("confirmed")?.value;
+    const isConfirmed = isConfirmedValue ? JSON.parse(isConfirmedValue) : false;
 
-    if (isAuth && request.nextUrl.pathname.match(routes)) {
+    if (isAuth && !isConfirmed && !pathname.match(/\/confirm/)) {
+        return NextResponse.redirect(new URL("/confirm", request.url));
+    }
+
+    if (isAuth && isConfirmed && pathname.match(/\/confirm/)) {
         return NextResponse.redirect(new URL("/", request.url));
     }
 
-    if (!isAuth && !request.nextUrl.pathname.match(routes)) {
+    if (isAuth && pathname.match(routes)) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    if (!isAuth && !pathname.match(routes)) {
         return NextResponse.redirect(new URL("/intro", request.url));
     }
 
