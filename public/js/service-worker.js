9165b2ec7fa010659dbc78e44208e6aacae32204
arperevalov <lovetraindriver@gmail.com>
2024-03-16 21:57:28 +0700

    .

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,18 +1,14 @@
-self.addEventListener('install', function() {
-});
+self.addEventListener("install", function () {});
 
-self.addEventListener('activate', function() {
-});
+self.addEventListener("activate", function () {});
 
-self.addEventListener('push', function(event) {
+self.addEventListener("push", function (event) {
     const data = event.data.text();
 
     const options = {
         body: data,
-        icon: '/favicons/android-chrome-72x72.png'
+        icon: "/favicons/android-chrome-72x72.png",
     };
 
-    event.waitUntil(
-        self.registration.showNotification("Friska", options)
-    );
+    event.waitUntil(self.registration.showNotification("Friska", options));
 });
