ebf86f17623e011000095ca70726e2a924e64df4
arperevalov <lovetraindriver@gmail.com>
2024-03-16 21:57:08 +0700

    feat: service-worker.js

---
+++ public/js/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function() {
+});
+
+self.addEventListener('activate', function() {
+});
+
+self.addEventListener('push', function(event) {
+    const data = event.data.text();
+
+    const options = {
+        body: data,
+        icon: '/favicons/android-chrome-72x72.png'
+    };
+
+    event.waitUntil(
+        self.registration.showNotification("Friska", options)
+    );
+});
