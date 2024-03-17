b404e6e25a04bfd8d10a516d496e5367813ac927
arperevalov <lovetraindriver@gmail.com>
2024-03-17 23:49:11 +0700

    feat: notifications api routes

---
+++ app/api/notifications/delete/route.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError } from "axios";
+import { cookies } from "next/headers";
+
+export async function POST(req: Request) {
+    const token = cookies().get("auth-token")?.value;
+    try {
+        const requestData = await req.json();
+        const request = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/notifications/delete`, requestData, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+
+        const data = await request.data;
+
+        return Response.json(data);
+    } catch (error: unknown) {
+        if (error instanceof AxiosError) {
+            return Response.json({ error: error }, { status: error.response?.status });
+        }
+    }
+}
