c7d1f8daf028fbe0658638e9b4d0b6ae33a1ead1
arperevalov <lovetraindriver@gmail.com>
2024-03-20 20:02:02 +0700

    feat: Enable notifications switch at settings

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -2,9 +2,11 @@
 
 import Header from "@/components/Header";
 import { SelectUnits } from "@/components/SelectUnits";
+import { Switch } from "@/components/Switch";
 import CardsStyle from "@/enums/CardsStyle";
 import Themes from "@/enums/Themes";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import useNotifications from "@/hooks/useNotifications";
 import useSettings from "@/hooks/useSettings";
 import Link from "next/link";
 import { FormEvent, ReactNode } from "react";
@@ -12,11 +14,26 @@ import { FormEvent, ReactNode } from "react";
 function Settings(): ReactNode {
     const { currentUser } = useCurrentUser();
     const { getThemeAction, setThemeAction, getCardsStyleAction, setCardsStyleAction } = useSettings();
+    const { subscribe, unsubscribe } = useNotifications();
     const capitalize = (item: string) => {
         return item.charAt(0).toUpperCase() + item.slice(1);
     };
     const themes = Object.keys(Themes).map((item) => capitalize(item));
     const cardsStyles = Object.keys(CardsStyle).map((item) => capitalize(item));
+
+    const onNotificationsSwitchChange = (event: FormEvent<HTMLInputElement>) => {
+        if (event.currentTarget.checked) {
+            subscribe();
+        } else {
+            unsubscribe();
+        }
+    };
+
+    const registerNotificationsSwitch = () => {
+        return {
+            onChange: onNotificationsSwitchChange,
+        };
+    };
 
     const onThemeSelectUpdate = (event: FormEvent<HTMLInputElement>) => {
         setThemeAction(event.currentTarget.value.toLowerCase());
@@ -50,6 +67,11 @@ function Settings(): ReactNode {
                             <div className="user-main__email">{currentUser.email}</div>
                         </div>
                     </div>
+                    <Switch
+                        label="Enable notifications"
+                        formKey="notifications"
+                        register={registerNotificationsSwitch}
+                    />
                     <SelectUnits
                         values={cardsStyles}
                         label="Display Cards As"
