92710dd85943a392ba005cea83467277506d2853
arperevalov <lovetraindriver@gmail.com>
2024-03-20 20:00:49 +0700

    feat: Switch component

---
+++ components/Switch.tsx
@@ -0,0 +1,17 @@
+interface SwitchInterface {
+    register: CallableFunction;
+    label: string;
+    formKey: string;
+    required?: boolean;
+    defaultChecked?: boolean;
+}
+
+export const Switch = (props: SwitchInterface) => {
+
+    const {register, formKey, required, label, defaultChecked} = props;
+
+    return <label htmlFor="" className="switch">
+        <input className="switch__input" type="checkbox" required={required} {...register(formKey)} defaultChecked={defaultChecked}/>
+        { label }
+    </label>
+}
